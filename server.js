a001957e01d6b79ffd7ed3e5d7c117a8140bca8c
pdbrooks <PDBrooks01@outlook.com>
2021-03-27 15:56:05 -0400

    Initial Structure Created

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+const session = require('express-session')
+
+//Database Setting
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+
+//Database Creation
+const mongoURI = process.env.MONGODBURI
+const db = mongoose.connection
+
+//Connecting mongo
+mongoose.connect(mongoURI, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, ()=>{
+    console.log("database connection checked...");
+})
+
+db.on('error', (err)=> { console.log('ERROR: ', err)});
+db.on('connected', ()=> { console.log("mongo connected")})
+db.on('disconnected', ()=> { console.log("mongo disconnected")})
+
+
+//CSS Connect
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: true}))
+
+//Sessions
+
+
+//Controllers
+
+
+
+app.listen(PORT, ()=>{
+  console.log('Server is ready to roll...');
+})
